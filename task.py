5edef8b43653e7b992eea362d788723c3acae63f
jakar02 <136799016+jakar02@users.noreply.github.com>
2023-11-05 22:25:08 +0100

    poprawka

--- task.py
+++ task.py
@@ -1,69 +1,145 @@
-#
-#AccountInfoing simulator. Write a code in python that simulates the AccountInfoing system. 
-#The program should:
-# - be able to create new AccountInfos
-# - store client information in AccountInfos
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+import logging
+import multiprocessing
+import threading
+import queue
+from queue import Empty
 
-class Bank:
-    def __init__(self, bankname):
-        self.bankname = bankname
+logging.basicConfig(filename='account_info.log', level=logging.INFO)
+
+class Account:
+    def __init__(self, account_id, name_surname):
+        self.account_id = account_id
+        self.name_surname = name_surname
+        self.money = 0
+
+    def deposit(self, money_to_deposit):
+        self.money = self.money + money_to_deposit
+
+    def withdraw(self, money_to_withdraw):
+        if self.money - money_to_withdraw >= 0:
+            self.money = self.money - money_to_withdraw
+        else:  
+            logging.error("Not enough money. You need %s more.", money_to_withdraw - self.money)
+
+    def transfer_money(self, account_to_transfer, money_to_transfer):
+        if self.money - money_to_transfer >= 0:
+            self.money = self.money - money_to_transfer
+            account_to_transfer.money = account_to_transfer.money + money_to_transfer
+        else:  
+            logging.error("Not enough money. You need %s more.", money_to_transfer - self.money)
+
+    def get_account_info(self):
+        return f'Account_id: {self.account_id}\nName Surname: {self.name_surname}\nMoney: {self.money}\n'
+
 
 class AccountInfo:
-    def __init__(self, name, cash_on_account, bankname):
-        self.name = name
-        self.cash_on_account = cash_on_account
-        Bank.bankname = bankname
+    number_of_accounts = 0
 
-    def __str__(self):
-        return 'Name: ' + str(self.name) + ', money: ' + str(self.cash_on_account) #+ str(Bank.bankname)
+    def __init__(self):
+        self.list_of_all_accounts = []
 
-    def cash_input(self, add_money):
-        self.cash_on_account = self.cash_on_account + add_money
+    @staticmethod
+    def sum_of_money_in_bank(list_of_everyone):
+        sum = 0
+        for account in list_of_everyone:
+            sum = sum + account.money
+        return sum
+    
+    def new_account(self, acc_id, name_surname):
+        if acc_id not in self.list_of_all_accounts:
+            new_account = Account(acc_id, name_surname)
+            self.list_of_all_accounts.append(new_account)
+            AccountInfo.number_of_accounts = AccountInfo.number_of_accounts + 1
+        else:
+            logging.error("Account with this ID already exsist.")
 
-    def send_money(self, how_much_money, clientX):
-        self.cash_on_account = self.cash_on_account-how_much_money
-        clientX.cash_on_account = clientX.cash_on_account + how_much_money
+    def get_account(self, id):
+        for account in self.list_of_all_accounts:
+            if id == account.account_id:
+                return account
+        else:
+            return None
+
+    def deposit(self, id, money_to_deposit):
+        account = self.get_account(id)
+        if account is not None:
+            account.deposit(money_to_deposit)
+        else:
+            logging.error("Account with ID %s not found.", id)
+            
+    def withdraw(self, id, money_to_withdraw):
+        account = self.get_account(id)
+        if account is not None:
+            account.withdraw(money_to_withdraw)
+        else:
+            logging.error("Account with ID %s not found.", id)
+
+    def transfer_money(self, id, id_to_send, money_to_transfer):
+        account = self.get_account(id)
+        account_to_send = self.get_account(id_to_send)
+        account.transfer_money(account_to_send, money_to_transfer)
+
+    def get_accounts_info(self):
+        for account in self.list_of_all_accounts:
+            logging.info("%s\n-------------------------", account.get_account_info())
+
+    def find_the_most_rich_person(func):
+        def inner(*args, **kwargs):
+            logging.info("Searching for the richest person...")
+            richest_person = func(*args, **kwargs)
+            return richest_person
+        return inner
+
+    @find_the_most_rich_person
+    def find(self):
+        richest_person = self.list_of_all_accounts[0]
+        for account in self.list_of_all_accounts:
+            if account.money > richest_person.money:
+                richest_person = account
+        return richest_person.name_surname, richest_person.money
 
 
 if __name__ == "__main__":
-    bank1 = Bank("BestBankEver")
+    obj = AccountInfo()
+    obj.new_account(1, "Adam Paleczny")
+    obj.new_account(2, "Witek Dabrawski")
+    obj.new_account(3, "Bartek Sekalski")
+    obj.get_accounts_info()
+    obj.deposit(2, 100)
+    obj.deposit(1, 50)
+    obj.deposit(3, 200)
+    obj.withdraw(1, 10)
+    obj.withdraw(1, 10)
+    obj.withdraw(2, 20)
+    obj.transfer_money(2,1,40)
+    obj.transfer_money(1,2,30)
+    obj.transfer_money(1,2,15)
+    obj.get_accounts_info()
+    logging.info(obj.sum_of_money_in_bank(obj.list_of_all_accounts))
+    name1, money1 = obj.find()
+    logging.info(f'Name: {name1}, money: {money1}')
+    logging.info(AccountInfo.number_of_accounts)
+    obj.withdraw(1, 1000)
 
-    client1 = AccountInfo("Jakub", 123, bank1)
-    print(client1)
-    client1.cash_input(12)
-    print(client1)
+    data = "4, Ambrozy Gajda\n5, Jackie Chan\n6, James Bond"
+    file_path = "data.txt"
+    with open(file_path, "w") as file:
+        file.write(data)
+    parsed_data = []
+    with open(file_path, "r") as file:
+        for line in file:
+            parts = line.strip().split(",")
+            obj.new_account(int(parts[0]), parts[1])
+    obj.get_accounts_info()
+    
+    deposit_thread = threading.Thread(target=obj.deposit, args=(4, 1000))
+    withdraw_thread = threading.Thread(target=obj.withdraw, args=(3, 20))
+    deposit_thread.start()
+    withdraw_thread.start()
+    deposit_thread.join()
+    withdraw_thread.join()
+    obj.get_accounts_info()
 
-    client2 = AccountInfo("Witek", 100, bank1)
-    print(client2)
-
-    client1.send_money(20, client2)
-    print(client1)
-    print(client2)
-
-
-
-
-
+    
 
 
